529fdb2206c6e24350489ee98287ba4ad3915214
matg0mes <hotmus8500@gmail.com>
2023-07-23 02:30:23 -0300

    feat(consumer): finish impl

---
+++ cmd/consumer/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "go-kafka-poc-kafka-1:9092",
+		"client.id":         "goapp-consumer",
+		"group.id":          "goapp.group",
+		"auto.offset.reset": "earliest",
+	}
+
+	c, err := kafka.NewConsumer(configMap)
+
+	if err != nil {
+		fmt.Println("Deu rum: ", err.Error())
+	}
+
+	topics := []string{"teste"}
+	c.SubscribeTopics(topics, nil)
+
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			fmt.Println(string(msg.Value), msg.TopicPartition)
+
+		}
+	}
+}
