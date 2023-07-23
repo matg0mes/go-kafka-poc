0b57070fbbe5e82f3150c95c12f61c8b6cb88160
matg0mes <hotmus8500@gmail.com>
2023-07-23 01:55:29 -0300

    feat(producer): finish impl

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	producer := NewKafkaProducer()
+
+	Publish("Mensagem", "teste", producer, nil)
+
+	producer.Flush(1000)
 }
 
 func NewKafkaProducer() *kafka.Producer {
 	configMap := &kafka.ConfigMap{
-		"bootstrap.server": "gokafka_kafka_1:9092",
+		"bootstrap.servers": "go-kafka-poc-kafka-1:9092",
 	}
 
 	p, err := kafka.NewProducer(configMap)
@@ -22,4 +25,20 @@ func NewKafkaProducer() *kafka.Producer {
 	}
 
 	return p
+}
+
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+	message := &kafka.Message{
+		Value:          []byte(msg),
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Key:            key,
+	}
+
+	err := producer.Produce(message, nil)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
